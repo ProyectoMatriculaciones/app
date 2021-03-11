b63e664bd2d49b8ecb8bd4ae00b0b742f9e5bb9e
JCapelDev <win200003@gmail.com>
2021-03-11 18:57:56 +0100

    Tabs predefined, initial design applied

--- www/js/init.js
+++ www/js/init.js
@@ -3,5 +3,19 @@
 
     $('.sidenav').sidenav();
 
+    //Posar les tabs
+    var options = { "swipeable": true };
+    var el = document.getElementById('tabs');
+    var instance = M.Tabs.init(el, options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('deviceready', onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+ 
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+}
