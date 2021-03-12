ad19dfddca41cdb649edc556d2121cc01dc8a20e
JCapelDev <win200003@gmail.com>
2021-03-12 15:48:52 +0100

    Automatic starter modal added

--- www/js/init.js
+++ www/js/init.js
@@ -8,14 +8,25 @@
     var el = document.getElementById('tabs');
     var instance = M.Tabs.init(el, options);
 
+    $(document).ready(function(){
+      $('.modal').modal();
+      $('.modal').modal('open');
+      
+    });
+    
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 document.addEventListener('deviceready', onDeviceReady, false);
+
  
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
  
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
+    // var instance = M.Modal.getInstance(elem);
+    //   instance.open();
+    
 }
