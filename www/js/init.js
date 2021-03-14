e0c0fc35a34e182f179728624b78bf3bec2ed399
JCapelDev <win200003@gmail.com>
2021-03-14 18:23:47 +0100

    config settings applied

--- www/js/init.js
+++ www/js/init.js
@@ -4,22 +4,28 @@
     $('.sidenav').sidenav();
 
     //Start de Swipeable tabs
-    var options = { "swipeable": true };
-    var el = document.getElementById('tabs');
-    var instance = M.Tabs.init(el, options);
+    // var options = { "swipeable": true };
+    // var el = document.getElementById('tabs');
+    // var instance = M.Tabs.init(el, options);
+    $('.tabs').tabs({ "swipeable": true });
 
-    $(document).ready(function(){
-      //Set parameters to the modal
-      $('.modal').modal({
-        dismissible: false
-      });
-      //Open at the start the login modal
-      $('.modal').modal('open');
-      
+    //(WIP) Set carousel tabs height to a 100% of the screen
+    // $(".carousel").style = "height: 100%;";
+    
+    //Set parameters to the modal
+    $('.modal').modal({
+      dismissible: false
     });
+    //Open at the start the login modal
+    $('.modal').modal('open');    
     
 
+    
+    
+   
+
   }); // end of document ready
+  
 })(jQuery); // end of jQuery name space
 
 document.addEventListener('deviceready', onDeviceReady, false);
@@ -33,4 +39,22 @@ function onDeviceReady() {
     // var instance = M.Modal.getInstance(elem);
     //   instance.open();
     
+    
 }
+
+function validateLogin(){
+    var query = $('#inputUsername').val();  
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:3000/" + query,
+        dataType: "json",
+      }).done(function(user) {
+        // DO STUFF HERE
+        
+      }).fail(function() {
+        alert("No se ha entrado ningun usuario con ese email.");
+      });
+
+}
+
+$("#signInButton").click(validateLogin)
